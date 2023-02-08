c2f09945c6cbfb6a4b76a9728f5ed18b30c40783
Fernando <cardarellafernando@gmail.com>
2023-02-08 12:28:47 -0300

    angular sin rutas

--- dynamic/portfolio/src/app/app.module.ts
+++ dynamic/portfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PanelComponent } from './components/panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
