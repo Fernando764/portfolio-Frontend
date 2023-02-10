7d3cc4d9274188227116f9e8f56912a088adde03
Fernando <cardarellafernando@gmail.com>
2023-02-10 10:00:17 -0300

    rutas creadas

--- dynamic/portfolio/src/app/app-routing.module.ts
+++ dynamic/portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PanelComponent } from './components/panel/panel.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'dash111', component: PanelComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ dynamic/portfolio/src/app/components/index.paginas.ts
@@ -0,0 +1,4 @@
+export { HomeComponent } from "./components/home.component";
+export { LoginComponent } from "./components/login.component";
+export { RegistroComponent } from "./components/registro.component";
+export { PanelComponent } from "./components/panel.component";
